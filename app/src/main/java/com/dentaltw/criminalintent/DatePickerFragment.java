8cca74488f710060363681cc81a31603da794a7e
Scott <mymislife@gmail.com>
2017-05-07 15:42:35 +0800

    add DatePicker

--- app/src/main/java/com/dentaltw/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/dentaltw/criminalintent/DatePickerFragment.java
@@ -4,7 +4,10 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
 
 /**
  * Created by Scott on 2017/5/7.
@@ -14,7 +17,10 @@ public class DatePickerFragment extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        return new AlertDialog.Builder(getActivity())
+        FragmentActivity activity = getActivity();
+        View view = LayoutInflater.from(activity).inflate(R.layout.dialog_date, null);
+        return new AlertDialog.Builder(activity)
+            .setView(view)
             .setTitle(R.string.date_picker_title)
             .setPositiveButton(android.R.string.ok, null)
             .create();
