ed9bc819cd9cbbf316b75e493eeb7718c40fcf23
Scott <mymislife@gmail.com>
2017-05-14 01:01:51 +0800

    Ch15 - Challenge: Use ShareCompat.IntentBuilder

--- app/src/main/java/com/dentaltw/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/dentaltw/criminalintent/CrimeFragment.java
@@ -11,6 +11,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.ShareCompat;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.text.format.DateFormat;
@@ -118,12 +119,12 @@ public class CrimeFragment extends Fragment {
         mReportButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(Intent.ACTION_SEND);
-                i.setType("text/plain");
-                i.putExtra(Intent.EXTRA_TEXT, getCrimeReport());
-                i.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.crime_report_subject));
-                i = Intent.createChooser(i, getString(R.string.send_report));
-                startActivity(i);
+                ShareCompat.IntentBuilder intent = ShareCompat.IntentBuilder.from(getActivity());
+                intent.setType("text/plain");
+                intent.setHtmlText(getCrimeReport());
+                intent.setSubject(getString(R.string.crime_report_subject));
+                intent.createChooserIntent();
+                intent.startChooser();
             }
         });
 
